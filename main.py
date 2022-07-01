b9f58fe67edd2bdcf4309b56c134dfccdffd352a
Innocent-Nyamusa <44573071+innocent-george-mithu-nyamusa@users.noreply.github.com>
2022-07-01 02:10:34 +0200

    added comments in file

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+#use python package for serial communication
 import serial
+#python package to interface with msysql server
 import mysql.connector
 
-
+#read serial from this port (COM8)
 port = serial.Serial('COM8',9600)
 
 while(True):
-#
+#execute for as long as possible 
+
+    #read from port 
     rfid = port.readline().decode('ascii')
     # print("out of this loop")
 
     if rfid:
-
+        #update value in mysql table
         mydb = mysql.connector.connect(host="localhost", user="root", database="elevator")
         mycursor = mydb.cursor()
         bill_query = "UPDATE cards_rfid SET status='active' WHERE card_rfid=%s"
